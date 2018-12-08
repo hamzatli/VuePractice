01ac6e8c8faaf76c73582a6987993cf97396be44
ismayil <ismayl.hemzetli@gmail.com>
2018-12-08 16:27:53 +0400

    Dynamic CSS

--- app.js
+++ app.js
@@ -2,19 +2,8 @@
 new Vue ({
 
   el:'#vue-app',
-  data: {
-    A: '',
-    B: ''
-
-  },
-
-  computed: {
-    Sum:function(){
-
-      return this.A + this.B
-
-    }
+  data:{
+    available: false
   }
-
 
 });
