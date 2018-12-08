000a46f46429b702f7541655f9a105128d75d472
ismayil <ismayl.hemzetli@gmail.com>
2018-12-08 13:16:46 +0400

    Simple method

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
   el: '#vue-app',
   data:{
-    name: 'Hello world'
+    name: 'İsmayıl'
+  },
+  methods: {
+    salute: function(time){
+
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
+
+
 });
